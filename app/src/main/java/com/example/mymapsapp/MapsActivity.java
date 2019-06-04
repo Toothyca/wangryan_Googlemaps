e2a87e028d1120d6bf7de44a3fc7a1fcaae05919
Toothyca <Toothyca@gmail.com>
2019-06-04 11:11:15 -0700

    Added ability to switch map style and markers for current location and birthplace

--- app/src/main/java/com/example/mymapsapp/MapsActivity.java
+++ app/src/main/java/com/example/mymapsapp/MapsActivity.java
@@ -1,7 +1,11 @@
 package com.example.mymapsapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +17,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private int mapType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +44,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng sandiego = new LatLng(32.986370, -117.023490);
+        mMap.addMarker(new MarkerOptions().position(sandiego).title("Marker in birthplace"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sandiego));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mapType = GoogleMap.MAP_TYPE_NORMAL;
     }
+
+    public void fun(View view)
+    {
+        if (mapType == 1)
+        {
+            mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+            mapType = GoogleMap.MAP_TYPE_TERRAIN;
+        }
+        else
+        {
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            mapType = GoogleMap.MAP_TYPE_NORMAL;
+        }
+    }
+
 }
